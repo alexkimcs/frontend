a7ebe7932a01af8060e9735d6c912ee69e52ffaa
crener <cole.rener@outlook.com>
2021-09-02 17:08:34 -0400

    cleaned up dummy text and added context and main component

---
+++ feedback-loop/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import { DataContext } from './hidden/DataContext';
+
+function Main(props) {
+    return (
+        <div>
+            <h1>main component</h1>
+        </div>
+    );
+}
+
+export default Main;
