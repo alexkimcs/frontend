996a0dd928a281c6a075598e30d031df13c2fb77
crener <cole.rener@outlook.com>
2021-09-03 10:12:25 -0400

    header is done

---
+++ feedback-loop/src/components/homeview/header/Searchbar.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useContext } from 'react';
+import { DataContext } from '../../hidden/DataContext';
+
+function Searchbar(props) {
+    const { interactionState, setInteractionState } = useContext(DataContext);
+
+    const [searchItem, setSearchItem] = useState('');
+    
+    const handleChange = (e) => {
+        setSearchItem(e.target.value);
+    }
+
+    const handleSubmit = () => {
+        setInteractionState({...interactionState, searchValue: searchItem});
+        setSearchItem('');
+    }
+    return (
+        <div className='Searchbar'>
+            <input className='searchbar-input' type='text' placeholder='search by: tags, posts, users' value={searchItem} onChange={handleChange} />
+            <button className='searchbar-submit' type='button' onClick={handleSubmit}>search</button>
+        </div>
+    );
+}
+
+export default Searchbar;
