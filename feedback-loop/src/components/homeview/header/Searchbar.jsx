9828b7dd664c45084926594cdd31ca68e14cd605
nlo88 <nloyeltylo@gmail.com>
2021-09-06 19:40:47 -0400

    searchbar fixed

--- feedback-loop/src/components/homeview/header/Searchbar.jsx
+++ feedback-loop/src/components/homeview/header/Searchbar.jsx
@@ -15,22 +15,16 @@ function Searchbar(props) {
         setSearchItem('');
     }
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-             <div class="container-fluid">
-             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-             <span class="navbar-toggler-icon"></span>
-             </button>
-        <div className='Searchbar'><span class='fas fa-search'></span>
-        <div className='Cancel-icon'><span class='fas fa-times'></span>
+      
+        <div className='Searchbar'>
+       
             <input className='searchbar-input' type='text' placeholder='search by: tags, posts, users' value={searchItem} onChange={handleChange} />
-            {/* <button className='searchbar-submit' type='button' onClick={handleSubmit}>SEARCH</button> */}
+            <button className='searchbar-submit' type='button' onClick={handleSubmit}><span class='fas fa-search'></span></button>
 
-            <button className='searchbar-submit' class='fas fa-search fa-1x ' type='button' onClick={handleSubmit}></button>
-
+            
         </div>
-        </div>
-        </div>
-        </nav>
+        
+        
     );
 }
 
