6680d2fcc5587d260f15ff99e102f7f634301c59
dunnoconnor <moconnor8@gmail.com>
2021-09-09 12:02:08 -0400

    hid url

--- src/App.js
+++ src/App.js
@@ -17,8 +17,9 @@ function App() {
     username: 'guest',
     userID: ''
   }
-  // const URL = "https://feedbackloopbackend.herokuapp.com"
-  const URL = "http://localhost:4000"
+
+  const URL = process.env.URL; 
+  //const URL = "http://localhost:4000"
   const [interactionState, setInteractionState] = useState(initialInteractionState);
   const [postsState, setPostsState] = useState(null);
   const [users, setUsers] = useState(null);
