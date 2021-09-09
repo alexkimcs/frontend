92293940b7466d614dc744e95026abc903a0170d
crener <cole.rener@outlook.com>
2021-09-09 13:07:20 -0400

    no results found message

--- src/components/homeview/feed/Feed.jsx
+++ src/components/homeview/feed/Feed.jsx
@@ -7,18 +7,31 @@ function Feed(props) {
     const { postsState } = useContext(DataContext);
 
     useEffect(() => {
+        console.log(postsState)
     }, [postsState])
 
+
+
     if (postsState) {
-        return (
-        <div className='Feed'>
-            {postsState.map((post) => {
-                return <PostCard
-                    key={post._id} post={post} 
-                />
-            })}
-        </div>
-        ) 
+        if (postsState.length === 0) {
+            return (
+                <div className='Feed-no-results'>
+                    <h3>no results found</h3>
+                    <h3>try another search</h3>
+                </div>
+            )
+        } else {
+           return (
+                <div className='Feed'>
+                    {postsState.map((post) => {
+                        return <PostCard
+                            key={post._id} post={post} 
+                        />
+                    })}
+                </div>
+            )  
+        }
+        
     } else {
         return (
         <div className='Feed'>
